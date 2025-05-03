3b0f65cd59c986aada3e74aa1e5b30266935f6a3
Mariam-Gappla <119626394+Mariam-Gappla@users.noreply.github.com>
2025-05-03 20:09:40 +0200

    edit redux

--- src/redux/store.jsx
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import InputReducer from "./inputslice";
-
-export const Store = configureStore({
-  reducer: {
-    input: InputReducer,
-  },
-});
